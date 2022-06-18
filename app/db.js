2aa296eb82cbb5c770f031a4376c0612822d7907
Leonardo David Mora <91294811+fafomax@users.noreply.github.com>
2022-06-18 02:40:52 -0300

    Update db.js
    Signed-off-by: Leonardo David Mora <91294811+fafomax@users.noreply.github.com>

--- app/db.js
+++ app/db.js
@@ -3,42 +3,42 @@ export const pizzas = [{
         nombre: "Promo",
         ingredientes: ["Salsa", "Queso", "Jamon", "Morron"],
         precio: 590,
-        img: "/Nucba/Tarea/Js/E4-LocalStorage+CSS/assets/1_pizzaPromo.jpg"
+        img: "/E4-LocalStorage+CSS/assets/1_pizzaPromo.jpg"
     },
     {
         id: 2,
         nombre: "4 Quesos",
         ingredientes: ["Salsa", "Mozzarella", "Roquefort", "Parmesano", "Tybo"],
         precio: 1000,
-        img: "/Nucba/Tarea/Js/E4-LocalStorage+CSS/assets/2_pizza4Quesos.jpg"
+        img: "/E4-LocalStorage+CSS/assets/2_pizza4Quesos.jpg"
     },
     {
         id: 3,
         nombre: "Pollo",
         ingredientes: ["Salsa", "Queso", "Pollo", "Crema"],
         precio: 970,
-        img: "/Nucba/Tarea/Js/E4-LocalStorage+CSS/assets/3_pizzaPollo.jpg"
+        img: "/E4-LocalStorage+CSS/assets/3_pizzaPollo.jpg"
     },
     {
         id: 4,
         nombre: "Tropical",
         ingredientes: ["Salsa", "Queso", "Jamon", "Cerezas", "Anana"],
         precio: 1050,
-        img: "/Nucba/Tarea/Js/E4-LocalStorage+CSS/assets/4_pizzaTropical.jpg"
+        img: "/E4-LocalStorage+CSS/assets/4_pizzaTropical.jpg"
     },
     {
         id: 5,
         nombre: "Puerro",
         ingredientes: ["Salsa", "Queso", "Oregano", "Puerro", "Panceta"],
         precio: 950,
-        img: "/Nucba/Tarea/Js/E4-LocalStorage+CSS/assets/5_pizzaPuerro.jpg"
+        img: "/E4-LocalStorage+CSS/assets/5_pizzaPuerro.jpg"
     },
     {
         id: 6,
         nombre: "Roquefort",
         ingredientes: ["Salsa", "Queso", "Roquefort"],
         precio: 1200,
-        img: "/Nucba/Tarea/Js/E4-LocalStorage+CSS/assets/6_pizzaRoquefort.jpg"
+        img: "/E4-LocalStorage+CSS/assets/6_pizzaRoquefort.jpg"
     },
 ];
 localStorage.setItem("pizzas", JSON.stringify(pizzas));
