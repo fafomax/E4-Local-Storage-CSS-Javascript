31a0f27577c9fe38d333501b2cd6e4b9f7208c2d
Leonardo David Mora <91294811+fafomax@users.noreply.github.com>
2022-06-18 02:48:40 -0300

    Update db.js
    Signed-off-by: Leonardo David Mora <91294811+fafomax@users.noreply.github.com>

--- app/db.js
+++ app/db.js
@@ -10,35 +10,35 @@ export const pizzas = [{
         nombre: "4 Quesos",
         ingredientes: ["Salsa", "Mozzarella", "Roquefort", "Parmesano", "Tybo"],
         precio: 1000,
-        img: "/E4-LocalStorage+CSS/assets/2_pizza4Quesos.jpg"
+        img: "./assets/2_pizza4Quesos.jpg"
     },
     {
         id: 3,
         nombre: "Pollo",
         ingredientes: ["Salsa", "Queso", "Pollo", "Crema"],
         precio: 970,
-        img: "/E4-LocalStorage+CSS/assets/3_pizzaPollo.jpg"
+        img: "./assets/3_pizzaPollo.jpg"
     },
     {
         id: 4,
         nombre: "Tropical",
         ingredientes: ["Salsa", "Queso", "Jamon", "Cerezas", "Anana"],
         precio: 1050,
-        img: "/E4-LocalStorage+CSS/assets/4_pizzaTropical.jpg"
+        img: "./assets/4_pizzaTropical.jpg"
     },
     {
         id: 5,
         nombre: "Puerro",
         ingredientes: ["Salsa", "Queso", "Oregano", "Puerro", "Panceta"],
         precio: 950,
-        img: "/E4-LocalStorage+CSS/assets/5_pizzaPuerro.jpg"
+        img: "./assets/5_pizzaPuerro.jpg"
     },
     {
         id: 6,
         nombre: "Roquefort",
         ingredientes: ["Salsa", "Queso", "Roquefort"],
         precio: 1200,
-        img: "/E4-LocalStorage+CSS/assets/6_pizzaRoquefort.jpg"
+        img: "./assets/6_pizzaRoquefort.jpg"
     },
 ];
 localStorage.setItem("pizzas", JSON.stringify(pizzas));
