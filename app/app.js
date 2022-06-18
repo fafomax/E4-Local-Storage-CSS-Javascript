ee8dc64a4db0379e72c25b1e9cb60a6693cd3ae5
fafomax <leonardodmora@gmail.com>
2022-06-18 02:22:32 -0300

    Ya Terminado

--- app/app.js
+++ app/app.js
@@ -1,64 +1,32 @@
+import {
+    pizzas
+} from "./db.js"
+
 const boton = document.getElementById("boton");
 const tituloPizza = document.getElementById("nombrePizza");
 const precioPizza = document.getElementById("precioPizza");
 const ingredientesPizza = document.getElementById("ingredientesPizza");
 const error1 = document.getElementById("error");
-const imgPizza = document.getElementById("#imgPizza");
+const imgPizza = document.getElementById("imgPizza");
 const inputNum = document.getElementById("numberFilter");
-const pizzas = [
-  {
-    id: 1,
-    nombre: "Promo",
-    ingredientes: ["salsa", "queso", "jamon", "morron"],
-    precio: 590,
-  },
-  {
-    id: 2,
-    nombre: "4 Quesos",
-    ingredientes: ["salsa", "mozzarella", "roquefort", "parmesano", "tybo"],
-    precio: 1000,
-  },
-  {
-    id: 3,
-    nombre: "Pollo",
-    ingredientes: ["salsa", "queso", "pollo", "crema"],
-    precio: 970,
-  },
-  {
-    id: 4,
-    nombre: "Tropical",
-    ingredientes: ["salsa", "queso", "jamon", "cerezas", "anana"],
-    precio: 1050,
-  },
-  {
-    id: 5,
-    nombre: "Puerro",
-    ingredientes: ["salsa", "queso", "oregano", "puerro", "panceta"],
-    precio: 950,
-  },
-  {
-    id: 6,
-    nombre: "Roquefort",
-    ingredientes: ["salsa", "queso", "roquefort"],
-    precio: 1200,
-  },
-];
 
 function buscadorPizzas() {
-  const value1 = parseInt(inputNum.value);
-  pizzas.forEach((pizzas) => {
-    if (value1 === pizzas.id) {
-      tituloPizza.innerHTML = pizzas.nombre;
-      precioPizza.innerHTML = "$" + pizzas.precio;
-      ingredientesPizza.innerHTML = pizzas.ingredientes.join(", ");
-      error1.innerHTML = "";
-    } else if (value1 > 5) {
-      error1.innerHTML = "Articulo no encontrado ";
-      tituloPizza.innerHTML = "";
-      precioPizza.innerHTML = "";
-      ingredientesPizza.innerHTML = "";
-    }
-  });
+    const value1 = parseInt(inputNum.value);
+    pizzas.forEach((pizzas) => {
+        if (value1 === pizzas.id) {
+            tituloPizza.innerHTML = pizzas.nombre;
+            precioPizza.innerHTML = "$" + pizzas.precio;
+            ingredientesPizza.innerHTML = pizzas.ingredientes.join(", ");
+            imgPizza.innerHTML = `<img src= "${pizzas.img}" alt="">`;
+            error1.innerHTML = "";
+        } else if (value1 > 5 || value1 == 0) {
+            imgPizza.innerHTML = `<img src= "/Nucba/Tarea/Js/E4-LocalStorage+CSS/assets/error.jpg" alt="">`;
+            error1.innerHTML = "Articulo no encontrado ";
+            tituloPizza.innerHTML = "";
+            precioPizza.innerHTML = "";
+            ingredientesPizza.innerHTML = "";
+        }
+    });
 }
 
 boton.addEventListener("click", buscadorPizzas);
---
+++ app/db.js
@@ -0,0 +1,44 @@
+export const pizzas = [{
+        id: 1,
+        nombre: "Promo",
+        ingredientes: ["Salsa", "Queso", "Jamon", "Morron"],
+        precio: 590,
+        img: "/Nucba/Tarea/Js/E4-LocalStorage+CSS/assets/1_pizzaPromo.jpg"
+    },
+    {
+        id: 2,
+        nombre: "4 Quesos",
+        ingredientes: ["Salsa", "Mozzarella", "Roquefort", "Parmesano", "Tybo"],
+        precio: 1000,
+        img: "/Nucba/Tarea/Js/E4-LocalStorage+CSS/assets/2_pizza4Quesos.jpg"
+    },
+    {
+        id: 3,
+        nombre: "Pollo",
+        ingredientes: ["Salsa", "Queso", "Pollo", "Crema"],
+        precio: 970,
+        img: "/Nucba/Tarea/Js/E4-LocalStorage+CSS/assets/3_pizzaPollo.jpg"
+    },
+    {
+        id: 4,
+        nombre: "Tropical",
+        ingredientes: ["Salsa", "Queso", "Jamon", "Cerezas", "Anana"],
+        precio: 1050,
+        img: "/Nucba/Tarea/Js/E4-LocalStorage+CSS/assets/4_pizzaTropical.jpg"
+    },
+    {
+        id: 5,
+        nombre: "Puerro",
+        ingredientes: ["Salsa", "Queso", "Oregano", "Puerro", "Panceta"],
+        precio: 950,
+        img: "/Nucba/Tarea/Js/E4-LocalStorage+CSS/assets/5_pizzaPuerro.jpg"
+    },
+    {
+        id: 6,
+        nombre: "Roquefort",
+        ingredientes: ["Salsa", "Queso", "Roquefort"],
+        precio: 1200,
+        img: "/Nucba/Tarea/Js/E4-LocalStorage+CSS/assets/6_pizzaRoquefort.jpg"
+    },
+];
+localStorage.setItem("pizzas", JSON.stringify(pizzas));
